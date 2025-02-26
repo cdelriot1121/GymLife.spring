43c2075e6f34073c3bbad48fe4bebbac04b88824
cdelriot1121 <carandresdelrio1121@gmail.com>
2025-02-25 23:57:51 -0500

    feat: ultima entrega

--- src/main/java/com/gymcj/gimnasio/controller/AdminController.java
+++ src/main/java/com/gymcj/gimnasio/controller/AdminController.java
@@ -1,8 +1,14 @@
 package com.gymcj.gimnasio.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AdminController {
+
+    @GetMapping("/admin/home")
+    public String Admin(){
+        return "admin";
+    }
 
 }
--- src/main/java/com/gymcj/gimnasio/controller/UsuarioController.java
+++ src/main/java/com/gymcj/gimnasio/controller/UsuarioController.java
@@ -1,8 +1,14 @@
 package com.gymcj.gimnasio.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UsuarioController {
+
+    @GetMapping("/usuario/home")
+    public String Usuarios(){
+        return "usuarios";
+    }
 
 }
