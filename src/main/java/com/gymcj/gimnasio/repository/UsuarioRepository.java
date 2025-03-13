163ee38ea94898c96cc16f9691292b067e9f3729
cdelriot1121 <carandresdelrio1121@gmail.com>
2025-03-12 22:35:30 -0500

    Feat: Solucion de roles para aquellos que se autentican con OAuth2.0 mediante google credentials. Ademas, se registran en la base de datos pero sin contraseña, todo eso se personaliza atravez de la nueva clase CustomOAuth2UserService

--- src/main/java/com/gymcj/gimnasio/repository/UsuarioRepository.java
+++ src/main/java/com/gymcj/gimnasio/repository/UsuarioRepository.java
@@ -9,4 +9,5 @@ import com.gymcj.gimnasio.entity.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     Optional<Usuario> findByUsername(String username);
+    boolean existsByUsername(String username);
 }
